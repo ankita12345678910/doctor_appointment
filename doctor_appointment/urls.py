921041f6c02f88c845a95d126fa3fcf43c12763e
ankita12345678910 <96244331+ankita12345678910@users.noreply.github.com>
2025-03-31 22:45:39 +0530

    Update home page UI, delete django toolbar

--- doctor_appointment/urls.py
+++ doctor_appointment/urls.py
@@ -21,5 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('appointmentapp.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
