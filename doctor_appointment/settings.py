ce5522450838fbb9ea60c5e8dcffcd094d051bf3
ankita12345678910 <96244331+ankita12345678910@users.noreply.github.com>
2025-03-31 22:45:39 +0530

    Update home page UI, delete django toolbar

--- doctor_appointment/settings.py
+++ doctor_appointment/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "appointmentapp",
-    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "doctor_appointment.urls"
