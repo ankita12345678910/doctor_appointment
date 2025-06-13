49427ae513ac39365531aa654a3d109a74fe3f2b
Anuradha Hazra <anuradhahazra713@gmail.com>
2025-06-13 10:51:11 +0530

    Update file structure

--- appointmentapp/urls.py
+++ appointmentapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 from .views import customLoginView
 
@@ -14,24 +14,14 @@ urlpatterns = [
 
 
 
-    
+
     # Common URL
     path('login/', customLoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'), 
-
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 
 
     # Patients URL
-    path("book/new/appointment", views.bookNewAppointment, name="book_appointment"),
-    path('cancel/appointment/<id>', views.cancelAppointment, name='cancel_appointment'),
-    path('ajax/fetch/time', views.ajaxFetchTime, name='ajax_fetch_time_from_date'),
-    path('ajax/fetch/appointment', views.ajaxFetchAppointment, name='ajax_fetch_appointment'),
-    path('test/home/', views.testHome, name='test_home'),
-    path('book/appointment/', views.bookAppointment, name='book_appointment'),
-    path('get-doctors/<int:spec_id>/', views.getDoctorsBySpecialization, name='get_doctors_by_specialization'),
-    path('get-doctor-form/<int:doctor_id>/', views.getDoctorForm, name='get_doctor_form'),  
-
-
+    path('patient/', include('appointmentapp.patient_urls')),
 
     # Doctor URL
     path('doctor/', include('appointmentapp.doctor_urls')),
